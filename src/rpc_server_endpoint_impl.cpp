73398a851a48beb028862f2292038f99f782df87
Takashi Mori <kanetugu2015@gmail.com>
2026-01-18 16:28:35 +0900

    refactored

--- src/rpc_server_endpoint_impl.cpp
+++ src/rpc_server_endpoint_impl.cpp
@@ -92,12 +92,12 @@ bool RpcServerEndpointImpl::initialize(const nlohmann::json& service_config, int
 
 void RpcServerEndpointImpl::pdu_recv_callback(const hakoniwa::pdu::PduResolvedKey& resolved_pdu_key, std::span<const std::byte> data) 
 {
-    std::cout << "INFO: Received PDU for service: " << resolved_pdu_key.robot << std::endl;
-    std::cout << " DEBUG: instance count: " << instances_.size() << std::endl;
+    //std::cout << "INFO: Received PDU for service: " << resolved_pdu_key.robot << std::endl;
+    //std::cout << " DEBUG: instance count: " << instances_.size() << std::endl;
     for (const auto& instance : instances_) {
         // robot_name = service_name
         if (instance->get_service_name() != resolved_pdu_key.robot) {
-            std::cout << "INFO: Skipping PDU for service: " << resolved_pdu_key.robot << ", does not match instance service: " << instance->get_service_name() << std::endl;
+            //std::cout << "INFO: Skipping PDU for service: " << resolved_pdu_key.robot << ", does not match instance service: " << instance->get_service_name() << std::endl;
             continue;
         }
         assert(instance->endpoint_ != nullptr);
@@ -108,7 +108,7 @@ void RpcServerEndpointImpl::pdu_recv_callback(const hakoniwa::pdu::PduResolvedKe
         pdu_data.resize(data.size());
         std::memcpy(pdu_data.data(), data.data(), data.size());
         instance->put_pending_request(pdu_key, pdu_data);
-        std::cout << "INFO: PDU stored for service: " << resolved_pdu_key.robot << std::endl;
+        //std::cout << "INFO: PDU stored for service: " << resolved_pdu_key.robot << std::endl;
         return;
     }
     std::cerr << "WARNING: Received PDU for unknown service: " << resolved_pdu_key.robot << std::endl;
@@ -145,7 +145,7 @@ ServerEventType RpcServerEndpointImpl::poll(RpcRequest& request)
         return handle_cancel_request(request);
     }
     else { // REQUEST
-        std::cout << "INFO: Received request for client: " << request.header.client_name << std::endl;
+        //std::cout << "INFO: Received request for client: " << request.header.client_name << std::endl;
         return handle_request_in(request);
     }
 }
@@ -164,7 +164,7 @@ void RpcServerEndpointImpl::send_reply(std::string client_name, const PduData& p
     
     server_states_[client_name].state = ServerState::SERVER_STATE_IDLE;
     server_states_[client_name].request_id = 0;
-    std::cout << "INFO: Reset state to IDLE for client: " << client_name << std::endl;
+    //std::cout << "INFO: Reset state to IDLE for client: " << client_name << std::endl;
 
     hakoniwa::pdu::PduKey pdu_key = {service_name_, client_name + "Res"};
     std::span<const std::byte> data(reinterpret_cast<const std::byte*>(pdu.data()), pdu.size());
@@ -173,7 +173,7 @@ void RpcServerEndpointImpl::send_reply(std::string client_name, const PduData& p
         std::cerr << "ERROR: Failed to send reply to client_name: " << client_name << ", error: " << static_cast<int>(error) << std::endl;
     }
     else {
-        std::cout << "INFO: Sent reply to client_name: " << client_name << std::endl;
+        //std::cout << "INFO: Sent reply to client_name: " << client_name << std::endl;
     }
 }
 
@@ -223,7 +223,7 @@ bool RpcServerEndpointImpl::validate_header(HakoCpp_ServiceRequestHeader& header
 
 ServerEventType RpcServerEndpointImpl::handle_request_in(RpcRequest& request) {
     if (server_states_[request.header.client_name].state == ServerState::SERVER_STATE_IDLE) {
-        std::cout << "INFO: Received request for client: " << request.header.client_name << std::endl;
+        //std::cout << "INFO: Received request for client: " << request.header.client_name << std::endl;
         server_states_[request.header.client_name].state = ServerState::SERVER_STATE_RUNNING;
         server_states_[request.header.client_name].request_id = request.header.request_id;
         request.client_name = request.header.client_name;
