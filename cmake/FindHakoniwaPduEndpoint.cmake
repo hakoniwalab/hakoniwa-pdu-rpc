7adb0099e4ab2c6f3a7f280a697e67b14f100c79
Takashi Mori <kanetugu2015@gmail.com>
2026-07-27 18:27:14 +0900

    Normalize legacy Endpoint discovery to package target

--- cmake/FindHakoniwaPduEndpoint.cmake
+++ cmake/FindHakoniwaPduEndpoint.cmake
@@ -1,17 +1,29 @@
+# Compatibility finder for Endpoint installations that predate the exported
+# hakoniwa_pdu_endpoint CMake package. Modern builds should resolve the package
+# config first and never enter this module.
+
+set(_HAKO_ENDPOINT_PREFIX_HINTS "")
+foreach(_value
+    "${HAKO_PDU_ENDPOINT_PREFIX}"
+    "${HAKONIWA_PDU_ENDPOINT_ROOT}"
+    "${HAKONIWA_PDU_ENDPOINT_DIR}"
+    "$ENV{HAKO_PDU_ENDPOINT_PREFIX}"
+    "$ENV{HAKO_PDU_ENDPOINT_ROOT}")
+  if(NOT "${_value}" STREQUAL "")
+    list(APPEND _HAKO_ENDPOINT_PREFIX_HINTS "${_value}")
+  endif()
+endforeach()
+if(NOT WIN32)
+  list(APPEND _HAKO_ENDPOINT_PREFIX_HINTS /usr/local/hakoniwa /usr/local /usr /opt/homebrew)
+endif()
+
 if(HAKO_PDU_ENDPOINT_INCLUDE_DIR)
   set(HAKONIWA_PDU_ENDPOINT_INCLUDE_DIR "${HAKO_PDU_ENDPOINT_INCLUDE_DIR}")
 else()
   find_path(HAKONIWA_PDU_ENDPOINT_INCLUDE_DIR
     NAMES hakoniwa/pdu/endpoint.hpp
-    PATHS
-      ${HAKO_PDU_ENDPOINT_PREFIX}
-      ${HAKONIWA_PDU_ENDPOINT_ROOT}
-      ${HAKONIWA_PDU_ENDPOINT_DIR}
-      /usr/local
-      /usr
-      /opt/homebrew
-    PATH_SUFFIXES
-      include
+    HINTS ${_HAKO_ENDPOINT_PREFIX_HINTS}
+    PATH_SUFFIXES include
   )
 endif()
 
@@ -20,16 +32,8 @@ if(HAKO_PDU_ENDPOINT_LIBRARY)
 else()
   find_library(HAKONIWA_PDU_ENDPOINT_LIBRARY
     NAMES hakoniwa_pdu_endpoint
-    PATHS
-      ${HAKO_PDU_ENDPOINT_PREFIX}
-      ${HAKONIWA_PDU_ENDPOINT_ROOT}
-      ${HAKONIWA_PDU_ENDPOINT_DIR}
-      /usr/local
-      /usr
-      /opt/homebrew
-    PATH_SUFFIXES
-      lib
-      lib64
+    HINTS ${_HAKO_ENDPOINT_PREFIX_HINTS}
+    PATH_SUFFIXES lib lib64 bin
   )
 endif()
 
@@ -38,19 +42,16 @@ find_package_handle_standard_args(HakoniwaPduEndpoint
   REQUIRED_VARS HAKONIWA_PDU_ENDPOINT_INCLUDE_DIR HAKONIWA_PDU_ENDPOINT_LIBRARY
 )
 
-if(HakoniwaPduEndpoint_FOUND AND NOT TARGET hakoniwa_pdu_endpoint)
-  add_library(hakoniwa_pdu_endpoint UNKNOWN IMPORTED)
-  set(_hako_pdu_endpoint_include_dirs "${HAKONIWA_PDU_ENDPOINT_INCLUDE_DIR}")
-  if(HAKO_PDU_ENDPOINT_PREFIX)
-    list(APPEND _hako_pdu_endpoint_include_dirs
-      "${HAKO_PDU_ENDPOINT_PREFIX}/include"
-      "${HAKO_PDU_ENDPOINT_PREFIX}/include/hakoniwa"
-    )
-  endif()
-  set_target_properties(hakoniwa_pdu_endpoint PROPERTIES
+if(HakoniwaPduEndpoint_FOUND
+   AND NOT TARGET hakoniwa_pdu_endpoint::hakoniwa_pdu_endpoint)
+  add_library(hakoniwa_pdu_endpoint::hakoniwa_pdu_endpoint UNKNOWN IMPORTED)
+  set_target_properties(hakoniwa_pdu_endpoint::hakoniwa_pdu_endpoint PROPERTIES
     IMPORTED_LOCATION "${HAKONIWA_PDU_ENDPOINT_LIBRARY}"
-    INTERFACE_INCLUDE_DIRECTORIES "${_hako_pdu_endpoint_include_dirs}"
+    INTERFACE_INCLUDE_DIRECTORIES "${HAKONIWA_PDU_ENDPOINT_INCLUDE_DIR}"
   )
 endif()
 
-mark_as_advanced(HAKONIWA_PDU_ENDPOINT_INCLUDE_DIR HAKONIWA_PDU_ENDPOINT_LIBRARY)
+mark_as_advanced(
+  HAKONIWA_PDU_ENDPOINT_INCLUDE_DIR
+  HAKONIWA_PDU_ENDPOINT_LIBRARY
+)
